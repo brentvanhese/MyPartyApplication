7d4f1b329af7b1a1e81a74fc3ac5d7b227ef56c8
brentvanhese <r0903817@student.thomasmore.be>
2022-02-26 14:56:22 +0100

    delete client

--- src/main/java/be/thomasmore/party2/repositories/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package be.thomasmore.party2.repositories;
-
-import be.thomasmore.party2.model.Client;
-import be.thomasmore.party2.model.Venue;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClientRepository extends CrudRepository<Client, Integer> {
-}
