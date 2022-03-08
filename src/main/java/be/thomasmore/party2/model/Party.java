5466e4fdd749713db075e3950180ae5803537e23
brentvanhese <r0903817@student.thomasmore.be>
2022-03-08 09:29:15 +0100

    create party entity

---
+++ src/main/java/be/thomasmore/party2/model/Party.java
@@ -0,0 +1,72 @@
+package be.thomasmore.party2.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Party {
+    @Id
+    private int id;
+    private String name;
+    private Integer pricePresaleInEur;
+    private Integer princeInEur;
+    private String extraInfo;
+    private Date date;
+    private Date doors;
+
+    public Party() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPricePresaleInEur() {
+        return pricePresaleInEur;
+    }
+
+    public void setPricePresaleInEur(Integer pricePresaleInEur) {
+        this.pricePresaleInEur = pricePresaleInEur;
+    }
+
+    public Integer getPrinceInEur() {
+        return princeInEur;
+    }
+
+    public void setPrinceInEur(Integer princeInEur) {
+        this.princeInEur = princeInEur;
+    }
+
+    public String getExtraInfo() {
+        return extraInfo;
+    }
+
+    public void setExtraInfo(String extraInfo) {
+        this.extraInfo = extraInfo;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Date getDoors() {
+        return doors;
+    }
+
+    public void setDoors(Date doors) {
+        this.doors = doors;
+    }
+}
