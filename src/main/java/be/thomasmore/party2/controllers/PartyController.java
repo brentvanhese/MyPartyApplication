ce8cd001835cb5fe7f75788b7f3fa528934fafc2
brentvanhese <r0903817@student.thomasmore.be>
2022-03-08 10:44:39 +0100

    create partydetails.html

--- src/main/java/be/thomasmore/party2/controllers/PartyController.java
+++ src/main/java/be/thomasmore/party2/controllers/PartyController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 public class PartyController {
@@ -18,5 +21,15 @@ public class PartyController {
         model.addAttribute("parties", allParties);
         model.addAttribute("nrParties", partyRepository.count());
         return "partylist";
+    }
+
+    @GetMapping({"partydetails", "/partydetails/{id}"})
+    public String partydetails(Model model, @PathVariable(required = false) Integer id) {
+        if (id==null) return "venuedetails";
+        Optional<Party> optionalParty = partyRepository.findById(id);
+        if (optionalParty.isPresent()) {
+            model.addAttribute("party", optionalParty.get());
+        }
+        return "partydetails";
     }
 }
