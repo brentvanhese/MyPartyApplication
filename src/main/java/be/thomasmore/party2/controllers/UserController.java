defe17160c77a296186164c75d28aff583f9bbcc
brentvanhese <r0903817@student.thomasmore.be>
2022-03-21 16:59:04 +0100

    stoped using standard login and logout pages

---
+++ src/main/java/be/thomasmore/party2/controllers/UserController.java
@@ -0,0 +1,29 @@
+package be.thomasmore.party2.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping("/login")
+    public String login(Principal principal, Model model) {
+        if (principal != null) return "redirect:/partylist";
+        return "user/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(Principal principal, Model model) {
+        if (principal == null) return "redirect:/partylist";
+        return "user/logout";
+    }
+}
+
